9ed30ed1b7a709c6fdb146f679fcd52dae274d74
NazilaHasanova04 <nazile.hesenova04@gmail.com>
2024-11-08 16:42:43 +0400

    Initial Commit

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your salary: ");
+            int salary = int.Parse(Console.ReadLine());
+
+            int annualSalary = salary * 12;
+
+            Console.WriteLine("Your annual salary is " + annualSalary);
+            Console.ReadKey();
+        }
+    }
+}
